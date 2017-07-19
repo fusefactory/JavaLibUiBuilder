b1577c3a54f9c5592cebe903d460de6e57d0080c
shortnotion (mark) <shortnotion@gmail.com>
2017-07-19 22:48:34 +0200

    NodeBuilder creates substructure

--- src/main/java/com/fuse/ui/builder/NodeBuilder.java
+++ src/main/java/com/fuse/ui/builder/NodeBuilder.java
@@ -1,18 +1,43 @@
 package com.fuse.ui.builder;
 
+import com.fuse.cms.Model;
 import com.fuse.cms.ModelCollection;
 import com.fuse.ui.Node;
 
 public class NodeBuilder {
+  private Instantiator instantiator;
   private ModelCollection collection;
   private String nodeId;
 
   public NodeBuilder(ModelCollection col, String nodeId){
     collection = col;
     this.nodeId = nodeId;
+    this.instantiator = new Instantiator();
   }
 
-  public Node getNode(){
-    return null;
+  public Node createNode(){
+    Model m = collection.findById(nodeId, true/* create-if-doesnt-exist */);
+    Node n = instantiator.createNode(m);
+
+    // create content of this node; loop over each child record in the collection
+    // if any is a _direct_ child of this node, build it, including it's sub-structure
+    // (recursive!) and add it to our node.
+    collection.each((Model mod) -> {
+      // child of us?
+      if(this.isDirectChild(mod, m)){
+        // create builder
+        NodeBuilder childBuilder = new NodeBuilder(this.collection, mod.getId());
+        // let builder generate node
+        Node childNode = childBuilder.createNode();
+        // add built node as child to our node
+        n.addChild(childNode);
+      }
+    });
+
+    return n;
+  }
+
+  private boolean isDirectChild(Model potentialChild, Model potentialParent){
+    return potentialChild.get("parent", "").equals(potentialParent.getId());
   }
 }
--- src/test/java/com/fuse/ui/builder/NodeBuilderTest.java
+++ src/test/java/com/fuse/ui/builder/NodeBuilderTest.java
@@ -5,17 +5,28 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
+import processing.core.PGraphics;
 import com.fuse.cms.ModelCollection;
+import com.fuse.ui.Node;
+import com.fuse.ui.TextNode;
+import com.fuse.ui.ImageNode;
 
 public class NodeBuilderTest {
   private ModelCollection col;
 
   public NodeBuilderTest(){
+    Node.setPGraphics(new PGraphics());
+
     col = new ModelCollection();
     col.loadJsonFromFile("testdata/NodeBuilderTest.json");
   }
-  @Test public void builder(){
-    NodeBuilder nb = new NodeBuilder(col, "");
-    assertFalse(nb.getNode() == null);
+
+  @Test public void createNode(){
+    NodeBuilder nb = new NodeBuilder(col, "NodeBuilderTest.page");
+    Node n = nb.createNode();
+    assertEquals(n.getChildNodes().get(0).getClass(), TextNode.class);
+    assertEquals(n.getChildNodes().get(1).getClass(), TextNode.class);
+    assertEquals(n.getChildNodes().get(2).getClass(), ImageNode.class);
+    assertEquals(n.getChildNodes().size(), 3);
   }
 }
