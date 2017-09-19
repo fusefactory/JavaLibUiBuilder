34a8edf740bb54a2f4180a058067d4097cb3b34d
Mark van de Korput <mark@fusefactory.it>
2017-09-19 17:08:56 +0200

    fialing tests for implicit node builder

---
+++ src/main/java/com/fuse/ui/builder/NodeBuilderImplicit.java
@@ -0,0 +1,9 @@
+package com.fuse.ui.builder;
+
+import com.fuse.cms.ModelCollection;
+
+public class NodeBuilderImplicit extends NodeBuilder {
+  public NodeBuilderImplicit(ModelCollection col, String nodeId){
+    super(col, nodeId);
+  }
+}
---
+++ testdata/NodeBuilderImplicitTest.java
@@ -0,0 +1,6 @@
+[
+    {"id": "NodeBuilderImplicitTest.page"},
+    {"id": "NodeBuilderImplicitTest.page.title", "type": "TextNode"},
+    {"id": "NodeBuilderImplicitTest.page.subtitle", "type": "TextNode"},
+    {"id": "NodeBuilderImplicitTest.page.image", "type": "ImageNode"},
+]
